e172ecf9fe28f665cfc5599dc131f74dd1c4a8bb
David Castro <65961492+castrolimoeiro@users.noreply.github.com>
2023-09-12 05:30:50 -0300

    Add files via upload

---
+++ pilha/src/one/digitalInovation/Pilha.java
@@ -0,0 +1,69 @@
+package one.digitalInovation;
+
+public class Pilha {
+	
+	private No refNoEntradaPilha;
+
+	public Pilha() {
+
+		this.refNoEntradaPilha = null;
+	}
+	
+	
+	public No pop() {
+		if(!isEmpty()) {
+			No noPoped = refNoEntradaPilha;
+			refNoEntradaPilha = refNoEntradaPilha.getRefNo();
+			return noPoped;		
+		}
+		return null;
+	}
+	
+	
+	public void push(No novoNo) {
+		No refAuxiliar = refNoEntradaPilha;
+		refNoEntradaPilha = novoNo;
+		refNoEntradaPilha.setRefNo(refAuxiliar);
+	}	
+	
+	public No top() {
+		return refNoEntradaPilha;
+	}
+	
+	public boolean isEmpty() {
+		/*if (refNoEntradaPilha == null) {
+			return true;
+		}
+		else 
+			return false;*/
+		
+		return refNoEntradaPilha == null? true : false;
+	}
+
+
+	@Override
+	public String toString() {
+		
+		String stringRetorno = "--------------------\n";
+		stringRetorno += "        Pilha\n";
+		stringRetorno += "--------------------\n";
+		
+		No noAuxiliar = refNoEntradaPilha;
+		
+		while(true) {
+			if(noAuxiliar != null) {
+				stringRetorno += "[No{dado= " + noAuxiliar.getDado() + "}]\n";
+				noAuxiliar = noAuxiliar.getRefNo();
+			}else {
+				break;
+			}
+			
+		
+		}
+		stringRetorno += "================\n";
+		return stringRetorno;
+	}
+	
+	
+
+}
